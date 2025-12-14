dac5db6f82f37dd6be86500e77b6814c3dce9971
Emmanuel-Odero <emmanuelodero808@gmail.com>
2025-12-14 15:43:12 +0300

    feat: add data models for 1815-api
    - Add Alias model for URL aliases
    - Add FeatureSubscription model
    - Add SearchHistory model
    - Add User model
    - Add models index file

---
+++ src/models/index.ts
@@ -0,0 +1,4 @@
+export { Alias, IAliasDocument } from './Alias';
+export { User, IUserDocument, INotificationPreferences } from './User';
+export { SearchHistory, ISearchHistoryDocument } from './SearchHistory';
+export { FeatureSubscription, IFeatureSubscriptionDocument } from './FeatureSubscription';
